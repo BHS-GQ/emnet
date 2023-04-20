c2802cd1abb59ced3d941117c6dac7938bf869da
Derick Wells <wowells@up.edu.ph>
2023-04-20 16:51:43 +0800

    Add named args to gen_tests.sh

--- gen_tests.sh
+++ gen_tests.sh
@@ -1,19 +1,47 @@
-# $1 tps "50 100 200"
-# $2 N "4 8 12"
-# $3 output_dir "_output"
-# $4 delay 20
-# $5 jitter 0
-# $6 rate 100kbit
+: '
+-t TPS "50 100 200"
+-n Ns "4 8 12"
+-o output_dir "_output"
+-d delay "20"
+-j jitter "0"
+-r rate "10mbit"
+'
+
+while getopts ":t:n:o:d:j:r:" opt; do
+  case $opt in
+    t) all_tps="$OPTARG"
+    ;;
+    n) all_n="$OPTARG"
+    ;;
+    o) output_dir="$OPTARG"
+    ;;
+    d) delay="$OPTARG"
+    ;;
+    j) jitter="$OPTARG"
+    ;;
+    r) rate="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
 
 
 CONSENSUS_ALGOS="hotstuff ibft qbft"
-for n in $2
+for n in $all_n
 do
     for algo in $CONSENSUS_ALGOS
     do
-        python3 -m generator.gen_network -c $algo -o $3 -t $1 -n $n --ip 172.16.239.
+        python3 -m generator.gen_network -c $algo -o $output_dir -t $all_tps -n $n --ip 172.16.239.
     done
 done
 
-target_dir="generator/"$3
-python3 -m generator.gen_runner -t $target_dir -d $4 -j $5 -r $6
+target_dir="generator/"$output_dir
+python3 -m generator.gen_runner -t $target_dir -d $delay -j $jitter -r $rate
