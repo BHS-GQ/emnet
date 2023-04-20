ab193c4d0f4e6980f250be02676948f32d0e6619
Derick Wells <wowells@up.edu.ph>
2023-04-20 10:22:45 +0800

    Initial commit

---
+++ generator/generate.py
@@ -0,0 +1,94 @@
+import os
+import argparse
+import json
+
+from pprint import pprint
+from pathlib import Path
+from glob import glob
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--consensus-algo', type=str, required=True)
+parser.add_argument('-n', '--n-validators', type=int, required=True)
+parser.add_argument('-o', '--output', type=Path, required=True)
+parser.add_argument('-t', '--tps', nargs='+', required=True)
+parser.add_argument('-ip', '--ip-base', type=str, default='172.16.239.')
+args = parser.parse_args()
+
+
+FILE_DIR = os.path.dirname(os.path.realpath(__file__))
+GENESIS_DIR = Path(f'{FILE_DIR}/genesis/{args.consensus_algo}_{args.n_validators}')
+BLS_KEY_DIR = Path(f'{FILE_DIR}/bls_keys/{args.n_validators}')  # Only used by hotstuff
+
+
+def get_val_info(args) -> dict:
+    # Generate val info
+    val_info = {}
+    for val_dir in glob(f'{GENESIS_DIR}/validator*'):
+        val_idx = int(val_dir.split('validator')[-1])
+        val_info[val_idx] = {}
+        for file in Path(val_dir).glob('*'):
+            fpath = Path(file)
+            val_info[val_idx][fpath.name] = fpath.read_text()
+    
+    # Generate IP addresses
+    for val_idx in range(args.n_validators):
+        val_info[val_idx]['ip'] = args.ip_base + str(val_idx + 5)
+
+    return val_info
+
+
+def build_static_nodes(val_info: dict) -> list:
+    # Build static-nodes.json
+    enodes = []
+    for val_dir in glob(f'{GENESIS_DIR}/validator*'):
+        val_idx = int(val_dir.split('validator')[-1])
+        nodekey_pub = val_info[val_idx]["nodekey.pub"]
+        ip = val_info[val_idx]["ip"]
+        enodes.append(
+            f'enode://{nodekey_pub}@{ip}:30303'
+        )
+
+    return enodes
+
+
+def build_genesis(args, val_info: dict) -> list:
+    genesis_file = GENESIS_DIR / Path("goQuorum/genesis.json")
+    with open(genesis_file, 'r', encoding='utf-8') as f:
+        genesis = json.load(f)
+
+        # Inject caliper contract deployer
+        genesis["alloc"]["0xfe3b557e8fb62b89f4916b721be55ceb828dbd73"] = {
+            "balance": "1000000000000000000000000000"
+        }
+
+        if args.consensus_algo == "hotstuff":
+            # Hotstuff uses QBFT genesis
+            if "qbft" in genesis["config"]:
+                del (genesis["config"]["qbft"])
+
+            genesis["mixHash"] = "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+            genesis["config"]["isQuorum"] = True
+            genesis["config"]["hotstuff"] = {
+                "requesttimeoutmilliseconds": 10000,  # Matches ibft, qbft default
+                "blockperiodseconds": 1,
+                "policy": "RoundRobin",
+                "faultymode": "Disabled",
+
+                # Get validator addresses
+                "validators": [
+                    f'0x{v["address"]}' for k, v in val_info.items()
+                ]
+            }
+
+    return genesis
+
+
+def main(args):
+    val_info = get_val_info(args)
+    enodes = build_static_nodes(val_info)
+    genesis = build_genesis(args, val_info)
+    pprint(genesis)
+
+if __name__ == '__main__':
+    main(args)
