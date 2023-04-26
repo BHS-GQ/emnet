47e74feebb470aaf8ceb3282466edf5b8685e2b4
Derick Wells <wowells@up.edu.ph>
2023-04-26 20:39:50 +0800

    Add docker monitoring in caliper

--- get_aws_reports.sh
+++ get_aws_reports.sh
@@ -11,6 +11,5 @@ ssh -i $AWS_PEM_FILE ubuntu@$AWS_IP "mkdir -p $3; cd $1; ./report_fetcher.sh $3"
 ssh -i $AWS_PEM_FILE ubuntu@$AWS_IP "mkdir -p $3; cd $1; ./logs_fetcher.sh $3"
 mkdir data/$2
 scp -i $AWS_PEM_FILE -r ubuntu@$AWS_IP:$3/* data/$2
-scp -i $AWS_PEM_FILE ubuntu@$AWS_IP:$1/*.log data/$2
 ssh -i $AWS_PEM_FILE ubuntu@$AWS_IP "rm -r $3"
 (cd data/$2 && mkdir -p logs && tar -xzvf logs.tar.gz -C logs && rm logs.tar.gz)
