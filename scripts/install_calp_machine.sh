09b099896190995cdac7da2c293ed5a4593d18e3
Glenn Lim <gvlim@up.edu.ph>
2023-05-12 12:26:43 +0800

    updated install_calp_machine

--- scripts/install_calp_machine.sh
+++ scripts/install_calp_machine.sh
@@ -1,6 +1,5 @@
 # Used to install dependencies in fresh EC2 instances
 
-
 sudo apt update
 
 # Install npm
@@ -9,13 +8,13 @@ sudo apt-get -y install npm
 
 # Install caliper-benchmarks
 git clone https://github.com/hyperledger/caliper-benchmarks
+git clone -b v5 --single-branch https://github.com/ndsg-eth1/caliper.git
 (
-    cd ~/caliper-benchmarks &&
-    npm init -y &&
-    npm install --only=prod @hyperledger/caliper-cli@0.4.2
-    npx caliper bind --caliper-bind-sut ethereum:latest
+    cd ~/caliper &&
+    npm i &&
+    npm run repoclean -- --yes &&
+    npm run bootstrap
 )
-
 # Install python
 sudo apt-get -y install python3-pip
 pip install PyYAML
