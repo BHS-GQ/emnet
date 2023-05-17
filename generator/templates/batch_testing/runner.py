c9fe0326b2bdc8432db2687e459b82dd16a38d64
Derick Wells <wowells@up.edu.ph>
2023-05-17 09:56:12 +0800

    Fix pruning

--- generator/templates/batch_testing/runner.py
+++ generator/templates/batch_testing/runner.py
@@ -14,22 +14,30 @@ def run_all():
     prev_consensus = ()
     test_dirs = glob('./*/')
     test_dirs.sort()
+    total_tests = 0
     for test_dir in test_dirs:
         td_path = Path(test_dir)
         td_full = td_path.resolve()
-        for idx in range(5): # todo: argparse
-            logging.info(f"Starting {td_full} Run {idx + 1}...")
+        
+        _dotenv_path = td_path / '.env'
+        _dotenv = dotenv_values(str(_dotenv_path.resolve()))
+        ca_id = (_dotenv['CONSENSUS_ALGO'], _dotenv['N_VALIDATORS'])
+        print(ca_id)
+        if prev_consensus != ca_id :
+            print("Pruning!")
+            subprocess.run(['docker', 'system', 'prune', '-f'], cwd=td_full)
+
+        runs = 5
+        for idx in range(runs): # todo: argparse
+            print(f"Starting {td_full} Run {idx + 1}...")
+            total_tests += 1
             # Timeout after 10 mins
             subprocess.run(['python3', 'test.py'], cwd=td_full, timeout=float(60*10))
             time.sleep(5)
 
-        _dotenv_path = td_path / '.env'
-        _dotenv = dotenv_values(str(_dotenv_path.resolve()))
-        ca_id = (_dotenv['CONSENSUS_ALGO'], _dotenv['N_VALIDATORS'])
-        if prev_consensus != ca_id :
-            subprocess.run(['docker', 'system', 'prune', '-f'], cwd=td_full)
-
         prev_consensus = ca_id
+    print(total_tests)
+
 
 if __name__ == "__main__":
     try:
