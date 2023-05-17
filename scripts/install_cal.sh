d7a1634cf44c91dddb711934d92c9e9bd647071c
Derick Wells <wowells@up.edu.ph>
2023-05-17 13:36:55 +0800

    Update benchmark install script

--- scripts/install_cal.sh
+++ scripts/install_cal.sh
@@ -6,14 +6,6 @@ sudo apt update
 sudo apt-get -y install nodejs
 sudo apt-get -y install npm
 
-# Install caliper-benchmarks
-git clone https://github.com/hyperledger/caliper-benchmarks
-(
-    cd ~/caliper-benchmarks &&
-    npm init -y &&
-    npm install --only=prod @hyperledger/caliper-cli@0.5.0
-    npx caliper bind --caliper-bind-sut ethereum:latest
-)
 git clone -b v5 --single-branch https://github.com/ndsg-eth1/caliper.git
 (
     cd ~/caliper &&
@@ -21,8 +13,10 @@ git clone -b v5 --single-branch https://github.com/ndsg-eth1/caliper.git
     npm run repoclean -- --yes &&
     npm run bootstrap
 )
-# Install python
-sudo apt-get -y install python3-pip
-pip install pip --upgrade
-pip install PyYAML
-pip install python-dotenv
+
+# Install caliper-benchmarks
+git clone https://github.com/hyperledger/caliper-benchmarks
+(
+    cd ~/caliper-benchmarks &&
+    node /home/ubuntu/caliper/packages/caliper-cli/caliper.js bind --caliper-bind-sut ethereum:1.3
+)
